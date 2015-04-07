bdff66b8735acfa4644394826a477bf5cd6054f4
ranveer raghuwanshi <ranveer.raghu@gmail.com>
2015-04-07 14:38:55 +0530

    Send Notification to Contacts and added support for sending NOTE

--- cartridge/static/default/js/pushbullet_bkup.js
+++ cartridge/static/default/js/pushbullet_bkup.js
@@ -2,77 +2,84 @@
 
 var ajax = require('./ajax');
 var sendToDevice = {
-	init: function () {
-		
+	init : function () {
+
 		//pushbullet Device List
-		$('.send-to-device').click(function(e){ 
+		$('.send-to-device').click(function (e) {
 			e.preventDefault();
-			alert('clicked');
 			sendToDevice.populatePushbulletDetails();
 		});
 
 		$('#pushbulletSubmit').on('click', function () {
-			console.log("jkjk");
 			sendToDevice.sendPushbulletData();
 		});
 	},
-	
-	
+
 	populatePushbulletDetails : function () {
-		// load card details
-		alert('populatePushbulletDetails');
 		var url = Urls.pushbulletGetUserDevices;
 		ajax.getJson({
-			url: url,
-			callback: function (data) {
-				if(typeof (data.error) == "undefined")
+			url : url,
+			callback : function (data) {
+				if (typeof(data.error) == "undefined")
 					sendToDevice.setpushbulletFields(data);
 				else
 					$('.pushbulletDeviceList .status').append(data.error.message);
-					
+
 			}
 		});
 	},
-	
+
 	sendPushbulletData : function () {
-		var deviceId = $('input:radio[id="pushbulletDevice"]:checked').attr('value');
+		
 		var linkToSend = $('#pushbulletLink').val();
 		var title = $('title').text();
-		alert(deviceId);
-		
-		var dataObject  = {
-				"deviceId" : deviceId,
-				"linkToSend" : linkToSend,
-				"title"      : title
+
+		var dataObject = {
+			"linkToSend" : linkToSend,
+			"title" : title
 		}
 		
+		if($('input:radio[id="pushbulletContact"]:checked').length){
+			dataObject.email = $('input:radio[id="pushbulletContact"]:checked').attr('value');
+			
+		}else{
+			dataObject.deviceId = $('input:radio[id="pushbulletDevice"]:checked').attr('value');
+			
+		}
+
 		var url = Urls.pushbulletSendNotification;
 		ajax.getJson({
-			url: url,
+			url : url,
 			data : dataObject,
-			callback: function (data) {
-				if(typeof (data.error) == "undefined")
+			callback : function (data) {
+				if (typeof(data.error) == "undefined")
 					$('.pushbulletDeviceList .status').html("").append("Successfull..!!!");
 				else
 					$('.pushbulletDeviceList .status').html("").append(data.error.message);
 			}
 		});
-		
-		
-		
+
 	},
-	
+
 	setpushbulletFields : function (data) {
-		alert('setpushbulletFields');
-		var devices = data.devices;
+		var devices = data.devices; 
+		var contacts = data.contacts;
 		var pbClass = ".pushbulletDeviceList";
-		devices.forEach(function(device){
-			
-			if(device.active){
-				var checkbox = '<input type="radio" id =pushbulletDevice name="pbdevices" value=' + device.iden + '>' + device.nickname + '<br>';
-				$(pbClass).prepend(checkbox);			
+		devices.forEach(function (device) {
+
+			if (device.active) {
+				var radiobutton = '<input type="radio" id =pushbulletDevice name="pbdevices" value=' + device.iden + '>' + device.nickname + '<br>';
+				$(pbClass).prepend(radiobutton);
 			}
 		});
+		contacts.forEach(function (contact) {
+
+			if (contact.active) {
+				var radiobutton = '<input type="radio" id =pushbulletContact name="pbdevices" value=' + contact.email + '>' + contact.name + " (Contact)" +  '<br>';
+				$(pbClass).prepend(radiobutton);
+			}
+		});
+		$('#pushbulletLink, #pushbulletSubmit').css('display', 'block')
 	}
 
 };
